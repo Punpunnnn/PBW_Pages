86c2e2f433df148ac48192ae50a7b379c3c8d4c1
Harfa Sabri <sabriharfa@gmail.com>
2023-04-30 08:29:48 +0700

    major Changes

--- animasi.js
+++ animasi.js
@@ -11,3 +11,16 @@ var icon = document.getElementById("icon");
      icon.src= "Assets/img/play.png";
    }
 }
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+const hiddenelements=document.querySelectorAll('.praktikum1,.praktikum2,.judul1,.judul2');
+hiddenelements.forEach((el) => observer.observe(el));
